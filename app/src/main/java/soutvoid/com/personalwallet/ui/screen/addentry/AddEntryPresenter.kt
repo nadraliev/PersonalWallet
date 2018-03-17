5490100542fe1698e7d4dad2b770555275882471
andrew <nadraliev@gmail.com>
2018-03-17 22:07:45 +0300

    started to implement add entry screen

--- app/src/main/java/soutvoid/com/personalwallet/ui/screen/addentry/AddEntryPresenter.kt
+++ app/src/main/java/soutvoid/com/personalwallet/ui/screen/addentry/AddEntryPresenter.kt
@@ -4,6 +4,8 @@ import com.arellomobile.mvp.InjectViewState
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.with
+import com.orhanobut.logger.Logger
+import io.realm.Realm
 import soutvoid.com.personalwallet.domain.transactionentry.EntryType
 import soutvoid.com.personalwallet.interactor.transactionentry.ICategoryRepository
 import soutvoid.com.personalwallet.ui.base.BasePresenter
@@ -13,6 +15,7 @@ class AddEntryPresenter(kodein: Kodein,
                         private val entryType: EntryType)
     : BasePresenter<AddEntryView>(kodein) {
 
+    val realm: Realm by with(this).instance()
     val categoryRepository: ICategoryRepository by with(this).instance()
 
     override fun onFirstViewAttach() {
@@ -20,5 +23,15 @@ class AddEntryPresenter(kodein: Kodein,
         viewState?.setToolbarColorForEntryType(entryType)
         viewState?.setStatusBarColorForEntryType(entryType)
         viewState?.setTitleForEntryType(entryType)
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        Logger.d(categoryRepository.getAll())
+    }
+
+    override fun onDestroy() {
+        realm.close()
+        super.onDestroy()
     }
 }
