285cb8945937b17b0ab8cb9875f7afe86557185c
andrew <soutvoid@gmail.com>
2018-05-02 11:20:23 +0300

    started to implement checks saving

--- app/src/main/java/soutvoid/com/personalwallet/ui/screen/checks/ChecksFragment.kt
+++ app/src/main/java/soutvoid/com/personalwallet/ui/screen/checks/ChecksFragment.kt
@@ -1,13 +1,15 @@
 package soutvoid.com.personalwallet.ui.screen.checks
 
+import android.support.v7.widget.Toolbar
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.PresenterType
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import soutvoid.com.personalwallet.R
 import soutvoid.com.personalwallet.app.App
 import soutvoid.com.personalwallet.ui.base.BaseFragment
+import soutvoid.com.personalwallet.ui.screen.main.IToolbarAdapter
 
-class ChecksFragment : BaseFragment(), ChecksView {
+class ChecksFragment : BaseFragment(), ChecksView, IToolbarAdapter {
 
     companion object {
         fun newInstance(): ChecksFragment = ChecksFragment()
@@ -21,4 +23,8 @@ class ChecksFragment : BaseFragment(), ChecksView {
             ChecksPresenter(App.instance.kodein)
 
     override fun getLayoutResId(): Int = R.layout.fragment_checks
+
+    override fun adaptToolbar(toolbar: Toolbar) {
+        activity?.title = getString(R.string.checks_title)
+    }
 }
